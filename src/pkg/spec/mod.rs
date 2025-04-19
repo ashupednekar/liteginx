d6de34d1f5925dde59879d26a6110cac801b9303
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-19 10:35:16 +0530

    feat: added spec and conf

---
+++ src/pkg/spec/mod.rs
@@ -0,0 +1,40 @@
+use serde::Deserialize;
+use crate::prelude::Result;
+
+
+#[derive(Debug, Deserialize)]
+pub enum Kind{
+    #[serde(alias="http")]
+    Http,
+    #[serde(alias="tcp")]
+    Tcp
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpstreamTarget{
+    pub host: String,
+    pub port: u16
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IngressSpec{
+    pub kind: Kind,
+    pub path: String,
+    pub listen: u16,
+    pub rewrite: Option<String>,
+    pub targets: Vec<UpstreamTarget>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TlsConf{
+    pub enabled: bool
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IngressConf{
+    pub name: String,
+    pub spec: Vec<IngressSpec>,
+    pub tls: TlsConf
+}
+
+
