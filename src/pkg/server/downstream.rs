39b7a9b4d0e712a2a67dad420efb663015b3e344
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-27 12:46:21 +0530

    feat: okay then, this is hard... but fun xP

--- src/pkg/server/downstream.rs
+++ src/pkg/server/downstream.rs
@@ -1,21 +1,21 @@
 use async_trait::async_trait;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{io::split, net::{TcpListener, TcpStream}};
 use crate::{pkg::spec::routes::{Route, UpstreamTarget}, prelude::{ProxyError, Result}};
 use rand::seq::IndexedRandom;
 
 #[async_trait]
 pub trait ListenDownstream{
     async fn serve(&self) -> Result<()>;
-    async fn handle(&self, conn: &'async_trait DownStreamConn) -> Result<()>;
+    async fn handle(&self, conn: &'async_trait mut DownStreamConn) -> Result<()>;
 }
 
 struct DownStreamConn<'a>{
     pub target: UpstreamTarget,
-    pub stream: &'a TcpStream
+    pub stream: &'a mut TcpStream
 }
 
 impl<'a> DownStreamConn<'a>{
-    pub fn new(stream: &'a TcpStream, targets: Vec<UpstreamTarget>) -> Result<Self> {
+    pub fn new(stream: &'a mut TcpStream, targets: Vec<UpstreamTarget>) -> Result<Self> {
         let target = match targets.choose(&mut rand::rng()) {
             Some(t) => t.clone(),
             None => {
@@ -32,13 +32,15 @@ impl ListenDownstream for Route{
     async fn serve(&self) -> Result<()> {
         let listener = TcpListener::bind(&format!("0.0.0.0:{}", &self.listen)).await?;
         loop{
-            let (stream, _) = listener.accept().await?;
-            let conn = DownStreamConn::new(&stream, self.targets.clone())?;
-            self.handle(&conn).await?;
+            let (mut stream, _) = listener.accept().await?;
+            let mut conn = DownStreamConn::new(&mut stream, self.targets.clone())?;
+            self.handle(&mut conn).await?;
         }
     }
 
-    async fn handle(&self, stream: &'async_trait DownStreamConn) -> Result<()>{
+    async fn handle(&self, conn: &'async_trait mut DownStreamConn) -> Result<()>{
+        let buffer = vec![1;1024];
+        let (mut reader, mut writer) = split(&mut conn.stream);
 
         Ok(())
     }
