568f8f15e4a31ed513f9eedca9a02bc1a0092237
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-27 12:36:24 +0530

    feat: async trait lifetimes dungeon entered successfully

--- src/pkg/server/downstream.rs
+++ src/pkg/server/downstream.rs
@@ -1,20 +1,20 @@
 use async_trait::async_trait;
 use tokio::net::{TcpListener, TcpStream};
 use crate::{pkg::spec::routes::{Route, UpstreamTarget}, prelude::{ProxyError, Result}};
-use rand::seq::{IndexedRandom, SliceRandom};
+use rand::seq::IndexedRandom;
 
 #[async_trait]
 pub trait ListenDownstream{
     async fn serve(&self) -> Result<()>;
-    async fn handle(&self, stream: TcpStream) -> Result<()>;
+    async fn handle(&self, conn: &'async_trait DownStreamConn) -> Result<()>;
 }
 
-struct DownstreamConnection<'a>{
+struct DownStreamConn<'a>{
     pub target: UpstreamTarget,
     pub stream: &'a TcpStream
 }
 
-impl<'a> DownstreamConnection<'a>{
+impl<'a> DownStreamConn<'a>{
     pub fn new(stream: &'a TcpStream, targets: Vec<UpstreamTarget>) -> Result<Self> {
         let target = match targets.choose(&mut rand::rng()) {
             Some(t) => t.clone(),
@@ -33,12 +33,12 @@ impl ListenDownstream for Route{
         let listener = TcpListener::bind(&format!("0.0.0.0:{}", &self.listen)).await?;
         loop{
             let (stream, _) = listener.accept().await?;
-            let conn = DownstreamConnection::new(&stream, self.targets.clone())?;
-            self.handle(stream).await?;
+            let conn = DownStreamConn::new(&stream, self.targets.clone())?;
+            self.handle(&conn).await?;
         }
     }
 
-    async fn handle(&self, stream: TcpStream) -> Result<()>{
+    async fn handle(&self, stream: &'async_trait DownStreamConn) -> Result<()>{
 
         Ok(())
     }
